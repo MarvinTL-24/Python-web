6b836fc91bda537fbeae1ec8854ceb4f3769e575
Marvin <escorpiano18@bol.com.br>
2024-11-28 16:26:39 -0300

    ok

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from django.urls import include, path
 
-# Register your models here.
+urlpatterns = [
+    path("polls/", include("polls.urls")),
+    path("admin/", admin.site.urls),
+]
