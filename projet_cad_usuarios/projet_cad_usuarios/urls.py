d11a9e607e763a22afd66fdbd0dd724088cdaa07
Marvin <escorpiano18@bol.com.br>
2024-12-02 14:34:10 -0300

    ok

--- projet_cad_usuarios/projet_cad_usuarios/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'usuarios'
-
-urlpatterns = [
-    path('', views.home, name='home'),
-    path('responder_enquete/', views.responder_enquete, name='responder_enquete'),
-]
